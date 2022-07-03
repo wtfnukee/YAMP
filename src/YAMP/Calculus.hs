ba25cbae3e552b4adc60c8391fc3fdfb10704677
wtfnukee <hehenukee@gmail.com>
2022-07-04 01:05:42 +0300

    Added note on type signature

--- src/YAMP/Calculus.hs
+++ src/YAMP/Calculus.hs
@@ -2,6 +2,9 @@ module Calculus
     (integrate
     ) where
 
+{- TODO: Better type signature
+ This type was automatically inferred by Haskell compiler.
+ It doesn't take into account that `n` should be a natural number-}
 integrate :: (Enum a, Fractional a) => (a -> a) -> a -> a -> a -> a
 integrate f a b n = ((sum $ map f [a+h, a + 2*h .. b-h]) + t) * h
           where t = (f a + f b) / 2
