1815ed35ee8da28e43b8e32dee2fdeca532e3a3c
wtfnukee <hehenukee@gmail.com>
2022-07-04 14:04:06 +0300

    Added Vectors

---
+++ src/YAMP/LinearAlgebra.hs
@@ -0,0 +1,12 @@
+module LinearAlgebra where
+
+-- Vectors are Column Vectors
+data Vector i = Vector [i] deriving Show
+
+instance (Eq i) => Eq (Vector i) where
+    (Vector v1) == (Vector v2) = v1 == v2
+instance (Num i, Floating i) => Num (Vector i) where
+    (Vector v1) + (Vector v2) = Vector (zipWith (+) v1 v2)
+    (Vector v1) - (Vector v2) = Vector (zipWith (-) v1 v2)
+    abs (Vector v) = Vector [sqrt $ sum $ map square v]
+        where square x = x * x
